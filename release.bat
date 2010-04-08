3606a9a438c45ea5e1252f038f4b8750872a378a
SND\terrymah_cp <SND\terrymah_cp@c102de6d-fbcd-4082-88ef-c2bab1378b82>
2010-04-08 23:30:35 +0000

    Updates
    git-svn-id: https://fairport.svn.codeplex.com/svn/fairport/trunk@39505 c102de6d-fbcd-4082-88ef-c2bab1378b82

--- release.bat
+++ release.bat
@@ -1,7 +1,18 @@
+:: This batch file generates a release for the PST File Format SDK. It must
+:: be run from the root of the trunk/branch you want to generate a release 
+:: for, or unexpected things will happen. "svn up" will be called.
 ::
-:: Update Everything
+:: To use it, type "release x_y_z" where x, y, and z form the version number
 ::
-cd fairport
+:: 7zip must be in your path
+:: sfk must be in your path
+:: svn must be in your path
+:: doxygen must be in your path
+
+::
+:: Update everyone, regenerate documentation
+::
+
 svn up
 doxygen Doxyfile
 cd ..
@@ -17,8 +28,15 @@ svn export fairport pstsdk_%1
 ::
 
 del pstsdk_%1\Doxyfile
+del pstsdk_%1\release.bat
 del /q pstsdk_%1\doc\*.pdf
 del pstsdk_%1\pstsdk\ndb\context.h
+
+::
+:: Convert everything to windows line endings
+::
+
+sfk addcr .h .cpp .txt
 
 ::
 :: Create release files
